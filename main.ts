6fd1f9b7565bae99df85fbc4155420bbd3bde95f
GulsunTuzun35 <77925175+GulsunTuzun35@users.noreply.github.com>
2021-02-07 23:18:44 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,50 @@
+input.onButtonPressed(Button.A, function () {
+    Tekrar_sayısí += 1
+    basic.showNumber(Tekrar_sayısí)
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < Tekrar_sayısí; index++) {
+        basic.showLeds(`
+            . . # . .
+            # # # # #
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # . .
+            . . # # #
+            . . # . .
+            . # # . .
+            . # . # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # . .
+            # # # . .
+            . . # . .
+            . . # # .
+            . # . # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . # # # .
+            . . # . .
+            . . # . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # . .
+            # # # # #
+            . . # . .
+            . # # # .
+            # . . . #
+            `)
+        basic.pause(100)
+    }
+})
+let Tekrar_sayısí = 0
+Tekrar_sayısí = 0
